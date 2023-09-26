72d19097fa215a63b6c88fd206d3a28d3435ac5c
PitchayaninM <pitchayaninmakoon@gmail.com>
2023-09-26 10:46:43 +0700

    update

--- src/PGSqlCRUD.js
+++ src/PGSqlCRUD.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-const dbUrl = ''
+const dbUrl = 'postgres://webadmin:AIIhrh85376@node50264-pitchayanin.proen.app.ruk-com.cloud:11546'
 
 const sequelize = new Sequelize(dbUrl);
